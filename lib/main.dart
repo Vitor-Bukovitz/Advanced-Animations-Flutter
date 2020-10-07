b30a72f993e9f0f7228f17528072e35e8290efed
Vitor Bukovitz <vitorbukovitz@hotmail.com>
2020-10-07 14:13:24 -0300

    sign in button animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:animations_app/screens/home_screen.dart';
 import 'package:animations_app/screens/login/login_screen.dart';
 import 'package:flutter/material.dart';
 
--- lib/screens/login/widgets/stagger_animation.dart
+++ lib/screens/login/widgets/stagger_animation.dart
@@ -3,8 +3,20 @@ import 'package:flutter/material.dart';
 class StaggerAnimation extends StatelessWidget {
   StaggerAnimation({
     @required this.controller,
-  });
+  }) : buttonSqueeze = Tween<double>(
+          begin: 320,
+          end: 60,
+        ).animate(
+          CurvedAnimation(
+            parent: controller,
+            curve: Interval(
+              0.0,
+              0.150,
+            ),
+          ),
+        );
   final AnimationController controller;
+  final Animation<double> buttonSqueeze;
 
   @override
   Widget build(BuildContext context) {
@@ -20,28 +32,40 @@ class StaggerAnimation extends StatelessWidget {
         bottom: 50,
       ),
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          controller.forward();
+        },
         child: Container(
-          alignment: Alignment.center,
-          width: 320,
-          height: 60,
-          decoration: BoxDecoration(
-            color: Color.fromRGBO(247, 64, 106, 1.0),
-            borderRadius: BorderRadius.all(
-              Radius.circular(30),
+            alignment: Alignment.center,
+            width: buttonSqueeze.value,
+            height: 60,
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(247, 64, 106, 1.0),
+              borderRadius: BorderRadius.all(
+                Radius.circular(30),
+              ),
             ),
-          ),
-          child: Text(
-            'Sign in',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.w300,
-              letterSpacing: 0.3,
-            ),
-          ),
-        ),
+            child: _buildInside(context)),
       ),
     );
+  }
+
+  Widget _buildInside(BuildContext context) {
+    if (buttonSqueeze.value > 75) {
+      return Text(
+        'Sign in',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.w300,
+          letterSpacing: 0.3,
+        ),
+      );
+    } else {
+      return CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+        strokeWidth: 1.0,
+      );
+    }
   }
 }
